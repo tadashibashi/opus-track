604c775c7a42817b65185bc3af7b5263ab4bc643
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-07-12 13:30:41 -0700

    implement name in User schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,11 @@
 import mongoose, {Schema} from "mongoose";
 
 export const userSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    }
 });
 
 const User = mongoose.model("User", userSchema);
