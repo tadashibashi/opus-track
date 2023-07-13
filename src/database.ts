13427218afdd4ccf7a9140cf8253dcf312c4504d
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-07-13 11:26:04 -0700

    remove grid-fs code

--- src/database.ts
+++ src/database.ts
@@ -1,24 +1,16 @@
 import mongoose from "mongoose";
-const Grid = require("gridfs-stream");
-Grid.mongo = mongoose.mongo;
-let gfs: any; // no types for this lib :/
 
 async function config() {
     const mongoURI = process.env["MONGO_URI"];
 
     if (typeof mongoURI === "string") {
         await mongoose.connect(mongoURI);
-        gfs = Grid(mongoose.connection.db);
+        return mongoose;
     } else {
         throw Error("database.config: MONGO_URI not found in .env file!");
     }
 }
 
-function getGfs() {
-    return gfs;
-}
-
 export default {
     config,
-    getGfs,
-};
+}
