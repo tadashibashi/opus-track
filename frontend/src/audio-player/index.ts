062ef309247c3ea03becc399646ac41f3fd5ea4e
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-07-13 04:04:21 -0700

    add timeout check to soundmanager2

--- frontend/src/audio-player/index.ts
+++ frontend/src/audio-player/index.ts
@@ -1,19 +1,33 @@
 import {AudioPlayer} from "./AudioPlayer";
 
+const SoundManagerTimeout = 10000;
+
 function preload() {
+
+    // SoundManager2 promise
     const soundManagerLoaded = new Promise((resolve, reject) => {
+        // check for timeout error
+        const timeout = setTimeout(() => {
+            reject(new Error("SoundManager2 load timeout."));
+        }, SoundManagerTimeout);
+
         soundManager.onready = () => {
+            soundManager.onready = undefined; // cleanup
+            clearTimeout(timeout);
             resolve(true);
         };
     });
 
+    // Window load promise
     const windowLoaded = new Promise((resolve, reject) => {
         let cb = () => {
-            window.removeEventListener("load", cb);
+            window.removeEventListener("load", cb); // cleanup
             resolve(true);
         };
         window.addEventListener("load", cb);
     });
+
+    // Wait for it...
     return Promise.all([windowLoaded, soundManagerLoaded]);
 }
 
