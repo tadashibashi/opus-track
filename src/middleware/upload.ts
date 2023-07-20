556a6235b06465e2541be66576973d28d45d07e7
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-07-19 22:57:41 -0700

    AWS s3 tested and working

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -2,32 +2,57 @@ import {Response, Request, NextFunction} from "express";
 import busboy from "busboy";
 import crypto from "crypto";
 import {s3} from "../util/aws";
+import path from "path";
 
 /**
- * This type appears inside req.body.files when uploadMultiple is placed before it
+ * This type appears inside req.body.files when upload is placed before it
  */
 export type FileInfo = busboy.FileInfo & { name: string };
 
-function uploadMultiple() {
+interface UploadConfig {
+    // Permissible mimetypes. Left unspecified will allow any type to upload.
+    mimeTypes?: string[],
+}
+
+export function upload(config?: UploadConfig) {
     return async function (req: Request, res: Response, next: NextFunction) {
         const infos: FileInfo[] = [];
 
         await new Promise((resolve, reject) => {
             const bb = busboy({headers: req.headers});
+
             bb.on("file", (name, file, info) => {
+                // guard against undesired mimeTypes
+                if (config && config.mimeTypes && !config.mimeTypes.includes(info.mimeType))
+                    return;
+
+                // upload to s3
                 s3.upload({
                     Bucket: "opustrack",
                     Body: file,
-                    Key: crypto.randomBytes(20).toString("hex")
-                });
-                infos.push(Object.assign({name}, info));
-            });
 
-            bb.on("error", (err) => reject(err));
-            bb.on("close", () => {
+                    Key: crypto.randomBytes(20).toString("hex") + path.extname(info.filename || ""),
+                }).send((err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        console.log("uploaded data: ", data);
+                        infos.push(Object.assign({name}, info));
+                    }
+                });
+
+            })
+            .on("error", (err) => reject(err))
+            .on("close", () => {
                 req.body.files = infos;
+                console.log("Busboy finished parsing form.");
                 resolve(true);
             });
-        }).catch(next);
+
+            req.pipe(bb);
+        })
+        .then(() => next())
+        .catch(next)
+
     }
 }
--- src/routes/files.ts
+++ src/routes/files.ts
@@ -1,9 +1,51 @@
 import express from "express";
 import {upload} from "../util/upload";
+import {upload as bbupload} from "../middleware/upload";
 import * as filesCtrl from "../controllers/files";
 
 const router = express.Router();
 
+router.post("/test", bbupload(), (req, res, next) => {
+    console.log(req.body);
+    res.end("file uploaded!");
+});
+
+import {s3} from "../util/aws";
+import {S3} from "aws-sdk";
+
+async function getFile(url: string) {
+    return new Promise<S3.GetObjectOutput>((resolve, reject) => {
+        s3.getObject({Bucket: "opustrack", Key: url}, (err, data) => {
+           if (err) {
+               reject(err);
+           }  else {
+               resolve(data);
+           }
+        });
+    });
+}
+
+router.get("/aws/:id", (req, res, next) => {
+    s3.getObject({Bucket: "opustrack", Key: req.params["id"]}, (err, data) => {
+        if (err) {
+            if (err.name === "NoSuchKey") {
+                res.json({success: false, message: "No file available"});
+                return;
+            }
+
+            next(err);
+            return;
+        }
+        if (!data.Body) {
+            res.json({success: false, message: "No file available"});
+            return;
+        }
+
+        console.log(data);
+
+        res.end(data.Body);
+    });
+});
 
 router.post("/upload", upload.single("file"), filesCtrl.create);
 router.get("/:id", filesCtrl.show);
